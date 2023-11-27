90a5eeda69fec37b2e8a2f47314f356a17721634
Anderson <uhuander@gmail.com>
2023-11-27 01:00:32 -0300

    done-responsive-mode

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ function menuShow (){
 
   if(menuMobile.classList.contains('open')){
     menuMobile.classList.remove('open');
+    document.querySelector('.icon').src='assets/hamburger-menu.svg';
   }else{
     menuMobile.classList.add('open');
+    document.querySelector('.icon').src='assets/close-button.svg';
   }
 
 
