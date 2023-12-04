66bf312210ed826a7183971570a23338c3474549
Anderson <uhuander@gmail.com>
2023-12-04 15:26:16 -0300

    correction-made-whatsappbutton-and-header

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
 function menuShow (){
   let menuMobile = document.querySelector('.mobile__navLinks');
+  let header = document.querySelector('header');
 
   if(menuMobile.classList.contains('open')){
     menuMobile.classList.remove('open');
+    header.classList.remove('open');
     document.querySelector('.icon').src='assets/hamburger-menu.svg';
   }else{
     menuMobile.classList.add('open');
+    header.classList.add('open');
     document.querySelector('.icon').src='assets/close-button.svg';
   }
 }
+
+
+
+// function menuShow() {
+//   let menuMobile = document.querySelector('.mobile__navLinks');
+//   let header = document.querySelector('header');
+
+//   if (menuMobile.classList.contains('open')) {
+//     menuMobile.classList.remove('open');
+//     document.querySelector('.icon').src = 'assets/hamburger-menu.svg';
+//     header.classList.remove('open'); // Remover a classe 'open' do header
+//   } else {
+//     menuMobile.classList.add('open');
+//     document.querySelector('.icon').src = 'assets/close-button.svg';
+//     header.classList.add('open'); // Adicionar a classe 'open' ao header
+//   }
+// }
