1ebfbc03a34d8e036295a97c198a64296e33ae53
Anderson <uhuander@gmail.com>
2023-12-05 15:53:36 -0300

    fix-responsive-mode

--- js/main.js
+++ js/main.js
@@ -15,17 +15,3 @@ function menuShow (){
 
 
 
-// function menuShow() {
-//   let menuMobile = document.querySelector('.mobile__navLinks');
-//   let header = document.querySelector('header');
-
-//   if (menuMobile.classList.contains('open')) {
-//     menuMobile.classList.remove('open');
-//     document.querySelector('.icon').src = 'assets/hamburger-menu.svg';
-//     header.classList.remove('open'); // Remover a classe 'open' do header
-//   } else {
-//     menuMobile.classList.add('open');
-//     document.querySelector('.icon').src = 'assets/close-button.svg';
-//     header.classList.add('open'); // Adicionar a classe 'open' ao header
-//   }
-// }
